4bb6bad0fa3808f3944e9b4ebad00a34905a2496
“ferretnh” <geordie.a.henderson+nicola@gmail.com>
2018-02-24 19:36:59 -0800

    Added ball class and created instance

--- bounce.py
+++ bounce.py
@@ -9,4 +9,15 @@ canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10,10,25,25, fill = color)
+        self.canvas.move(self.id, 245, 100)
+
+    def draw(self):
+        pass
+
+ball = Ball(canvas, 'red')
+
 tk.mainloop()
